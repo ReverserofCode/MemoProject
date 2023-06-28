d58cafb7b4ea6a57bcfc11185376f706ed801327
Administrator <patteon@naver.com>
2023-06-28 21:24:39 +0900

    프로젝트가 실행되는 지점에서 JPAAuditing 기능이 작동하지 않아 시간을 기록하는 변수가 작동하지 않아 기능을 추가하였다.

--- src/main/java/com/example/memo/MemoApplication.java
+++ src/main/java/com/example/memo/MemoApplication.java
@@ -2,10 +2,11 @@ package com.example.memo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MemoApplication.class, args);
 	}
